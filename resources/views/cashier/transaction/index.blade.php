7bf0379f28313c7720525c34d193212f9ef2758e
Eduardo <picis_9716@hotmail.com>
2024-05-30 22:44:00 -0500

    DEMO - SISTEMAS DE PEDIDOS V.1.0

--- resources/views/cashier/transaction/index.blade.php
+++ resources/views/cashier/transaction/index.blade.php
@@ -46,7 +46,7 @@
                         <!-- Row start -->
                         <div class="row">
                             <div class="col-md-3">
-                                <a class="btn btn-info" href="">ACTUALIZAR</a>
+                                <a class="boton-redondo mb-3" href="">ACTUALIZAR</a>
                             </div>
                             <div class="col-xl-12">
                                 @if (session()->has('message'))
